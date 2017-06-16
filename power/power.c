9275a846019ed0b3c862f4da92a1b4c97fad0e24
Wanja Schulze <rajaschu@googlemail.com>
2017-06-16 14:32:48 +0200

    power: alignment changes
    https://github.com/msm7x30/android_device_qcom_msm7x30-common/commit/82c9380a28cafb2ff689a102b04e9d2cf9fdc1df#diff-6b5df4d6dd7fd500f298fb1e4abc0e7a

--- power/power.c
+++ power/power.c
@@ -171,7 +171,7 @@ static int boostpulse_open(struct cm_power_module *cm)
 }
 
 static void cm_power_hint(struct power_module *module, power_hint_t hint,
-                            void *data)
+                          void *data)
 {
     struct cm_power_module *cm = (struct cm_power_module *) module;
     char buf[80];
@@ -179,31 +179,34 @@ static void cm_power_hint(struct power_module *module, power_hint_t hint,
 
     switch (hint) {
 #ifndef NO_TOUCH_BOOST
-    case POWER_HINT_INTERACTION:
+        case POWER_HINT_INTERACTION:
 #endif
-    case POWER_HINT_CPU_BOOST:
-        if (boostpulse_open(cm) >= 0) {
-            snprintf(buf, sizeof(buf), "%d", 0);
-            len = write(cm->boostpulse_fd, buf, strlen(buf));
+        case POWER_HINT_CPU_BOOST:
+            if (boostpulse_open(cm) >= 0) {
+                if (data != NULL)
+                    duration = (int) data;
 
-            if (len < 0) {
-                strerror_r(errno, buf, sizeof(buf));
-                ALOGE("Error writing to boostpulse: %s\n", buf);
+                snprintf(buf, sizeof(buf), "%d", duration);
+                len = write(cm->boostpulse_fd, buf, strlen(buf));
 
-                pthread_mutex_lock(&cm->lock);
-                close(cm->boostpulse_fd);
-                cm->boostpulse_fd = -1;
-                cm->boostpulse_warned = 0;
-                pthread_mutex_unlock(&cm->lock);
+                if (len < 0) {
+                    strerror_r(errno, buf, sizeof(buf));
+                    ALOGE("Error writing to boostpulse: %s\n", buf);
+
+                    pthread_mutex_lock(&cm->lock);
+                    close(cm->boostpulse_fd);
+                    cm->boostpulse_fd = -1;
+                    cm->boostpulse_warned = 0;
+                    pthread_mutex_unlock(&cm->lock);
+                }
             }
-        }
-        break;
+            break;
 
-    case POWER_HINT_VSYNC:
-        break;
+        case POWER_HINT_VSYNC:
+            break;
 
-    default:
-        break;
+        default:
+            break;
     }
 }
 
