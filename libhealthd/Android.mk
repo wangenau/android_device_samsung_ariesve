f5bf48a4aeb677cad766dd2f0c1659cf1e1982be
Wanja Schulze <rajaschu@googlemail.com>
2018-02-17 17:55:22 +0100

    Upstream changes
    -Improved struct initialization in sensors.cpp
    - Renamed local libhealthd.qcom to libhealtd.ariesve to fix errors with double module definition
    -Updated libhealthd
    
    https://github.com/DafabHoid/android_device_samsung_ariesve/commits/cm-13.0

--- libhealthd/Android.mk
+++ libhealthd/Android.mk
@@ -17,6 +17,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_ariesve.cpp
-LOCAL_MODULE := libhealthd.qcom
+LOCAL_MODULE := libhealthd.ariesve
 LOCAL_C_INCLUDES := system/core/healthd
 include $(BUILD_STATIC_LIBRARY)
